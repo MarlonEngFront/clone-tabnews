8343f705ac79cbcfe84bdeeb45ace1283b1564fa
MARLON DE ANDRADE DE SOUSA <marlon.aswa@gmail.com>
2023-07-27 16:10:55 +0000

    teste-page-video

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
+ import Link from 'next/link'
+
  function Home() {
     return (
+        <>
         <h1>Clone Tabnews</h1>
+        <Link href="/menu">
+        ir para o menu
+      </Link>
+        </>
     )
  }
 
