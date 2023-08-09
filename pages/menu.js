ff05d34c224982d38d944aac178b92268375912a
MARLON DE ANDRADE DE SOUSA <marlon.aswa@gmail.com>
2023-08-09 17:37:34 +0000

    adiciona arquivo ´.editorconfig´

--- pages/menu.js
+++ pages/menu.js
@@ -4,25 +4,6 @@ export default function menuPage() {
   return (
     <>
       <h1>Menu</h1>
-      <div style={{width: "100%", height: "150px"}}>
-      <div style={{maxWidth: "100%", margin: "0 auto" }}>
-        <div style={{ position: "relative", paddingBottom: "56.25%", height: 0 }}>
-          <embed
-            src="https://now-fe-next.hlg-netnow.net/embed/2145937"
-            width="100%"
-            height="100%"
-            style={{ position: "absolute", top: 0, left: 0 }}
-          />
-          <a
-            href="https://www.clarotvmais.com.br/"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%" }}
-          >
-          </a>
-        </div>
-      </div>
-      </div>
     </>
   );
 }
