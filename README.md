34cf35505e08521b09910d2cf563f8624f5f38b7
MARLON DE ANDRADE DE SOUSA <marlon.aswa@gmail.com>
2023-08-11 21:33:22 +0000

    adicionar scrips `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do Tabnews.com.br em um clone
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
- import Link from 'next/link'
+import Link from "next/link";
 
- function Home() {
-    return (
-        <>
-        <h1>Clone Tabnews</h1>
-        <Link href="/menu">
-        ir para o menu
-      </Link>
-        </>
-    )
- }
+function Home() {
+  return (
+    <>
+      <h1>Clone Tabnews</h1>
+      <Link href="/menu">ir para o menu</Link>
+    </>
+  );
+}
 
- export default Home;
+export default Home;
